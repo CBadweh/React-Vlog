75521718abbc97cd19e0f20890559589812a4bb8
C Badweh <sheenaymudwe@gmail.com>
2024-10-25 02:13:27 -0400

    add single post component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import TopBar from "./components/tobar/TopBar"
 import Home from "./pages/home/Home"
+import Single from "./pages/single/Single";
 
 function App() {
   return (
     // react fragments
     <> 
       <TopBar/> 
-      <Home/>
+      {/* <Home/> */}
+      <Single/>
     </>
   );
 }
