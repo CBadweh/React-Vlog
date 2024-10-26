9af4daac51246283d6aae7ecffeb16df5813ea2a
C Badweh <sheenaymudwe@gmail.com>
2024-10-25 23:44:43 -0400

    Add user login logics to Topbar navigation behavior

--- src/App.js
+++ src/App.js
@@ -9,18 +9,20 @@ import Register from "./pages/register/Register";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-
 function App() {
+  // Add User Login or Not Logic for Routine different pages
+  const user = true;
   return (
-    // chage the react fragments to Router
     <Router> 
       <TopBar/> 
       <Routes>
         <Route exact path="/" element={<Home/>} />
-        <Route path="/register" element={<Register/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/write" element={<Write/>} />
-        <Route path="/settings" element={<Settings/>} />
+        {/* if user logins, reroute <Register> page to <Home/> */}
+        <Route path="/register" element={user? <Home/> : <Register/>} /> 
+        <Route path="/login" element={user? <Home/> : <Login/>} />
+        {/* if user logins, route to <Write> else reroute to <Register> */}
+        <Route path="/write" element={user? <Write/> : <Register/>} /> 
+        <Route path="/settings" element={user? <Settings/> : <Register/>} />
         <Route path="/post/:postID" element={<Single/>} />
       </Routes>
 
--- src/components/tobar/TopBar.jsx
+++ src/components/tobar/TopBar.jsx
@@ -1,33 +1,47 @@
+import { Link } from "react-router-dom";
 import "./topbar.css"
 
 export default function TopBar() {
+  // Add User Login or Not Logic for different Headers
+  const user = true;
   return (
     <div className="top">
-        <div className="topLeft"> 
-            <i className="topIcon fab fa-facebook-square"></i>
-            <i className="topIcon fab fa-instagram-square"></i>
-            <i className="topIcon fab fa-pinterest-square"></i>
-            <i className="topIcon fab fa-twitter-square"></i>
-        </div>
+      <div className="topLeft"> 
+        <i className="topIcon fab fa-facebook-square"></i>
+        <i className="topIcon fab fa-instagram-square"></i>
+        <i className="topIcon fab fa-pinterest-square"></i>
+        <i className="topIcon fab fa-twitter-square"></i>
+      </div>
 
-        <div className="topCenter">
+      <div className="topCenter">
+        {/* Rerouse Header to specified pages */}
+        <ul className="topList">
+          <li className="topListItem"><Link className="link" to="/">HOME</Link></li>
+          <li className="topListItem"><Link className="link" to="/">ABOUT</Link></li>
+          <li className="topListItem"><Link className="link" to="/">CONTACT</Link></li>
+          <li className="topListItem"><Link className="link" to="/write">WRITE</Link></li>
+          <li className="topListItem">{user && "LOGOUT"}</li> {/* If there's a user loggined, display LOGOUT, else shows nothing */}
+        </ul>
+      </div>
+
+      <div className="topRight"> 
+        {/* Display IMG or LOGIN REGISTER dependinon if user logins */}
+        {user ? (
+            <Link className="link" to="/settings">
+              <img
+                className="topImg"
+                src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                alt=""
+              />
+            </Link>
+          ) : (
             <ul className="topList">
-                <li className="topListItem">HOME</li>
-                <li className="topListItem">ABOUT</li>
-                <li className="topListItem">CONTACT</li>
-                <li className="topListItem">WRITE</li>
-                <li className="topListItem">LOGOUT</li>
+              <li className="topListItem"><Link className="link" to="/login">LOGIN</Link></li>
+              <li className="topListItem"><Link className="link" to="/register">REGISTER</Link></li>
             </ul>
-        </div>
-
-        <div className="topRight"> 
-            <img
-              className="topImg"
-              src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-              alt=""
-            />
-            <i className="topSearchIcon fas fa-search"></i>
-        </div>
+        )}
+        <i className="topSearchIcon fas fa-search"></i>
+      </div>
     </div>
   )
 }
