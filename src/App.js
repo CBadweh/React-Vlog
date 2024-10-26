0862be52f73b26f3cc719e15515f383d9e9b24d0
C Badweh <sheenaymudwe@gmail.com>
2024-10-25 22:02:31 -0400

    Change React Fragment to React Route in App.js

--- src/App.js
+++ src/App.js
@@ -6,18 +6,25 @@ import Settings from "./pages/settings/Settings";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+
 function App() {
   return (
-    // react fragments
-    <> 
+    // chage the react fragments to Router
+    <Router> 
       <TopBar/> 
-      {/* <Home/> */}
-      {/* <Single/> */}
-      {/* <Write/> */}
-      {/* <Settings/> */}
-      {/* <Login/> */}
-      <Register/>
-    </>
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/write" element={<Write/>} />
+        <Route path="/settings" element={<Settings/>} />
+        <Route path="/post/:postID" element={<Single/>} />
+      </Routes>
+
+    </Router>
   );
 }
 
