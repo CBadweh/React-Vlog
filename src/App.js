8013420686bd0b407b3caf095a95583cc8f0c43e
C Badweh <sheenaymudwe@gmail.com>
2024-10-25 21:03:04 -0400

    add Login and Register  component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Home from "./pages/home/Home"
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
 import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 function App() {
   return (
@@ -12,7 +14,9 @@ function App() {
       {/* <Home/> */}
       {/* <Single/> */}
       {/* <Write/> */}
-      <Settings/>
+      {/* <Settings/> */}
+      {/* <Login/> */}
+      <Register/>
     </>
   );
 }
